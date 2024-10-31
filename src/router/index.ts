0379e584f27a701611b8c90f46307d51b57faeec
OeunSochetra <oeunsochetra@gmail.com>
2024-10-31 10:04:57 +0700

    "Removed import of useAuthStore and uncommented related code in router/index.ts"

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,47 +1,47 @@
 import { createRouter, createWebHistory } from "vue-router";
 import RouteName from "../constants/router-name";
-import { useAuthStore } from "../store/auth";
+// import { useAuthStore } from "../store/auth";
 
-const routes = [
-  {
-    path: "/",
-    name: RouteName.HOME,
-    component: () => import("../views/Home.vue"),
-    meta: { requiresAuth: true },
-  },
-  {
-    path: "/sign_in",
-    name: RouteName.SIGN_IN,
-    component: () => import("../views/authentication/sign_in.vue"),
-  },
-  {
-    path: "/sign_up",
-    name: RouteName.SIGN_UP,
-    component: () => import("../views/authentication/sign_up.vue"),
-  },
-  {
-    path: "/product",
-    name: RouteName.PRODUCT,
-    component: () => import("../views/Product.vue"),
-    meta: { requiresAuth: true },
-  },
-];
 const router = createRouter({
   history: createWebHistory(),
-  routes,
+  routes: [
+    {
+      path: "/",
+      name: RouteName.HOME,
+      component: () => import("../views/Home.vue"),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: "/sign_in",
+      name: RouteName.SIGN_IN,
+      component: () => import("../views/authentication/sign_in.vue"),
+    },
+    {
+      path: "/sign_up",
+      name: RouteName.SIGN_UP,
+      component: () => import("../views/authentication/sign_up.vue"),
+    },
+    {
+      path: "/product",
+      name: RouteName.PRODUCT,
+      component: () => import("../views/Product.vue"),
+      meta: { requiresAuth: true },
+    },
+  ],
 });
 
 // Global route guard to protect routes that require authentication
 router.beforeEach((to, from, next) => {
-  const authStore = useAuthStore();
+  // const authStore = useAuthStore();
+  // authStore.accessToken ||
   const token = localStorage.getItem("accessToken");
-  // console.log("Is Authenticated:", authStore.isAuthenticated);
 
+  // Check if route requires authentication and if the user is authenticated
   if (to.meta.requiresAuth && !token) {
-    // If the route requires auth and the user is not authenticated, redirect to login
-    next({ path: RouteName.SIGN_IN });
+    // Redirect to the sign-in page if not authenticated
+    next({ name: RouteName.SIGN_IN });
   } else {
-    next(); // Proceed to the route
+    next(); // Proceed to the route if authenticated or no auth required
   }
 });
 
